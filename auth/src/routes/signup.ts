6c7fc0d4303a5676c7d41cc5dc7c5da3259b67ee
Paolo <pieffelleo@gmail.com>
2022-04-03 15:00:55 +0200

    verification middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadReqestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -16,13 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be 4 to 20 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existUser = await User.findOne({ email });
