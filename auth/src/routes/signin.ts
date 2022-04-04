48264acd2a7cb0f5bc50adfcc6d8683be2b5d24e
Paolo <pieffelleo@gmail.com>
2022-04-04 19:19:12 +0200

    ingress yaml nwe with apply 41.2

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadReqestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -14,7 +19,37 @@ router.post(
       .withMessage('You must provide a password'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadReqestError('Invalid credentialss');
+    }
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadReqestError('Invalid credentialss');
+    }
+    // create a jwt
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // store jwt in session object
+    //--> the object below is made this way because of typescript interface
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
