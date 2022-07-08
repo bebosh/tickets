c90614780a45b17e3d1d40a1e152c8de915fd565
Paolo <pieffelleo@gmail.com>
2022-07-08 09:09:27 +0200

    created tickets directory

---
+++ tickets/src/index.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('listen on port 3000');
+  });
+};
+start();
